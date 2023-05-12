43d63c3fa1c0d72e248258bdc3adad1f30524170
Francois Gindraud <francois.gindraud@inria.fr>
2023-05-12 15:59:19 +0200

    C++ version fix ?
    Fix for macos, but using the generic build_ext from pybind11 helpers

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from pybind11.setup_helpers import Pybind11Extension
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
 setup (
     # Code content ; Pybind11Extension handles finding the lib wherever it has been installed
     ext_modules = [
         Pybind11Extension (
-            "apt.eigen_wrapper",
-            sources = ["apt/eigen_wrapper.cpp"],
-            include_dirs = ["include/"],
+            'apt.eigen_wrapper',
+            sources = ['apt/eigen_wrapper.cpp'],
+            include_dirs = ['include/'],
         )
-    ]
+    ],
+    cmdclass = { 'build_ext': build_ext } # Auto use latest C++ https://pybind11.readthedocs.io/en/stable/compiling.html
 )
